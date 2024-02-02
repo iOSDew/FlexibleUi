c41925ef219717f50b26938b6f08a3f1d31efb10
Hojimurod <hojimurodsultonov2004@gmail.com>
2024-02-02 23:30:07 +0500

    Initial Commit

---
+++ FlexibleUi/FlexibleUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexibleUiApp.swift
+//  FlexibleUi
+//
+//  Created by Khojimurod Sultonov on 02.02.24.
+//
+
+import SwiftUI
+
+@main
+struct FlexibleUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
